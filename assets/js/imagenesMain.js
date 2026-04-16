2c720ed6f05919f8e97c5508ef66ad1498e9df01
steve <steve@DORAIGAL>
2026-04-16 14:31:32 -0600

    se argega nueva imagenes a la pagina de servicios y se elimina el mapa de google maps

---
+++ assets/js/imagenesMain.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $.getJSON("https://raw.githubusercontent.com/Doraigal/TareaARG/refs/heads/main/assets/Json/imagenes.json",
+        function (data) {
+            $.each(data.dise, function (i, proyectos) {
+                $(".serProyectos").append(`
+                    <div class="cartas col-md-4 mb-5 card">
+                        <img src="${proyectos.imagen}" alt="${proyectos.nombre}" class="proyectos card-img"></img>
+                            <b class="card-body">
+                                ${proyectos.nombre}
+                            </b>
+                            <button class="btn btn-primary card-footer">Solicitar</button>
+                    </div>
+                `);
+            });
+        });
+    });
