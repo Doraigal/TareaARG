082858940a70e6e55657dcc0586a9ad7dc16c417
steve <steve@DORAIGAL>
2026-04-16 15:13:06 -0600

    cambios varios

--- assets/js/Proyectos.js
+++ assets/js/Proyectos.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
     $.getJSON("https://raw.githubusercontent.com/Doraigal/TareaARG/refs/heads/main/assets/Json/imagenes.json",
         function (data) {
-            $.each(data.diseñosUnicos, function (i, proyectos) {
-                $("#DiseñosUnicos").append(`
-                    <div class="col-md-6 d-flex justify-content-center align-items-center">
-                            <img src="${proyectos.imagen}" alt="Fusion" class="Fusion">
-                        </div>
+            $.each(data.proyectos, function (i, proyectos) {
+                $(".serProyectos").append(`
+                         <div class="cartas col-md-4 mb-5 card">
+                        <img src="${proyectos.imagen}" alt="${proyectos.nombre}" class="proyectos card-img"></img>
+                            <b class="card-body">
+                                ${proyectos.nombre}
+                            </b>
+                            <button class="btn btn-primary card-footer">Solicitar</button>
+                    </div>
                 `);
             });
         });
--- assets/js/imagenesMain.js
+++ assets/js/imagenesMain.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
     $.getJSON("https://raw.githubusercontent.com/Doraigal/TareaARG/refs/heads/main/assets/Json/imagenes.json",
         function (data) {
-            $.each(data.dise, function (i, proyectos) {
-                $(".serProyectos").append(`
-                    <div class="cartas col-md-4 mb-5 card">
-                        <img src="${proyectos.imagen}" alt="${proyectos.nombre}" class="proyectos card-img"></img>
-                            <b class="card-body">
-                                ${proyectos.nombre}
-                            </b>
-                            <button class="btn btn-primary card-footer">Solicitar</button>
-                    </div>
+            $.each(data.diseñosUnicos, function (i, proyectos) {
+                $("#DiseñosUnicos").append(`
+                   <div class="mt-5 col-md-6 d-flex justify-content-center align-items-center">
+                            <img src="${proyectos.imagen}" alt="Fusion" class="Fusion">
+                        </div>
                 `);
             });
         });
