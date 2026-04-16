4c3dcb09821c933182a02a23398723ce163ccab9
steve <steve@DORAIGAL>
2026-04-16 14:19:15 -0600

    se agregan funciones para mostrar los proyectos en la pagina de servicios, se agregan los proyectos a un array y se muestran en la pagina, se agrega un mapa con un marcador en la ubicacion de ARG Impresiones 3D, IA

--- assets/js/Proyectos.js
+++ assets/js/Proyectos.js
@@ -1,16 +1,16 @@
-document.ready(function () {
-    $.getJSON("", data,
-        function (data, textStatus, jqXHR) {
-            
-        }
-    );
-    $.ajax({
-        type: "method",
-        url: "url",
-        data: "data",
-        dataType: "dataType",
-        success: function (response) {
-
-        }
+$(document).ready(function () {
+    $.getJSON("https://raw.githubusercontent.com/Doraigal/TareaARG/refs/heads/main/assets/Json/imagenes.json",
+        function (data) {
+            $.each(data.proyectos, function (i, proyectos) {
+                $(".serProyectos").append(`
+                    <div class="cartas col-md-4 mb-5 card">
+                        <img src="${proyectos.imagen}" alt="${proyectos.nombre}" class="proyectos card-img"></img>
+                            <b class="card-body">
+                                ${proyectos.nombre}
+                            </b>
+                            <button class="btn btn-primary card-footer">Solicitar</button>
+                    </div>
+                `);
+            });
+        });
     });
-});
