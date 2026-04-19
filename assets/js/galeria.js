c9c5a7d6e9245c7420ea3d6fc9ea865c0209ac86
steve <steve@DORAIGAL>
2026-04-19 08:23:18 -0600

    se agragan imagenes al json

--- assets/js/galeria.js
+++ assets/js/galeria.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
     $.getJSON("https://raw.githubusercontent.com/Doraigal/TareaARG/refs/heads/main/assets/Json/imagenes.json",
         function (data) {
-            $.each(data.proyectos, function (i, proyectos) {
+            $.each(data.Galeria, function (i, Galeria) {
                 $(".Galeria").append(`
                          <div class="cartas col-md-3 mx-3 card m-0 p-0">
-                        <img src="${proyectos.imagen}" alt="${proyectos.nombre}" class="proyectos card-img"></img>
+                        <img src="${Galeria.imagen}" alt="${Galeria.nombre}" class="proyectos card-img"></img>
                             <b class="card-body">
-                                ${proyectos.nombre}
+                                ${Galeria.nombre}
                             </b>
-                            <button class="btn btn-primary card-footer p-0 m-0">Solicitar</button>
                     </div>
                 `);
             });
