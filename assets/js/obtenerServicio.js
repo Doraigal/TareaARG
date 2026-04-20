ca8884bbcd968b1150d2e854353e83f38057d3a0
steve <steve@DORAIGAL>
2026-04-20 01:00:56 -0600

    mejoras de codigo

--- assets/js/obtenerServicio.js
+++ assets/js/obtenerServicio.js
@@ -1,5 +1,6 @@
 $(document).on("click", ".btnComando", function() {
   const texto = $(this).siblings(".titulo").text();   // obtenemos el texto del div
-  $(".modal-title").text(texto);            // lo pasamos al input del form
+  $(".modal-title").text(texto);
+  $(".titul").val(texto).prop("disabled", true);           // lo pasamos al input del form
 });
 
