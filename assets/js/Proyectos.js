7d76e3a269f018841e0b011e209de91b52673550
Steven Arguello Vindas <stevenalber0396@hotmail.com>
2026-04-19 18:45:03 -0600

    cambios en services

--- assets/js/Proyectos.js
+++ assets/js/Proyectos.js
@@ -3,15 +3,17 @@ $(document).ready(function () {
         function (data) {
             $.each(data.proyectos, function (i, proyectos) {
                 $(".serProyectos").append(`
-                         <div class="cartas col-md-3 mx-3 card m-0 p-0">
-                        <img src="${proyectos.imagen}" alt="${proyectos.nombre}" class="proyectos card-img"></img>
+                    <div class="col-md-3 justify-content-center">
+                        <div class="cartas card m-0 p-0">
+                            <img src="${proyectos.imagen}" alt="${proyectos.nombre}" class="card-img-top proyectos card-img"></img>
                             <b class="card-body titulo">
                                 ${proyectos.nombre}
                             </b>
                             <button class="btnComando btn btn-primary card-footer p-0 m-0" type="button"
-                             data-bs-toggle="modal" data-bs-target="#myModal">Solicitar</button>
+                                data-bs-toggle="modal" data-bs-target="#myModal">Solicitar</button>
+                        </div>
                     </div>
                 `);
             });
         });
-    });
+});
